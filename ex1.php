1e1d90c02a9e56dd9fe5fbb866673c722ed6e35d
Carina Jurgovsky <carina.jurgovsky@gmx.at>
2020-07-13 15:57:12 +0200

    php day 1

---
+++ ex1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Day 1</title>
+	<link rel="stylesheet" type="text/css" href="styles_php1.css">
+</head>
+<body>
+
+	<?php
+	$name = "Carina";
+
+	echo "<h1>$name</h1>";
+
+	$d=date("D");
+	if ($d=="Mon")
+		echo "Did you have a good start into the week?";
+	else
+		echo "Have a wonderful week!"
+
+	?>
+
+</body>
+</html>
